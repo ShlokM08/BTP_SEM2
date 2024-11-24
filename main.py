d1e33605e7ebfe15d328d9d6a1042f6a37e7d299
ShlokM08 <shlok21421@iiitd.ac.in>
2024-11-25 00:06:25 +0530

    bg_darkblue

--- main.py
+++ main.py
@@ -266,7 +266,7 @@ def initialize_database():
 
 def main():
     # Set background image and custom styles
-    set_background("assets/Untitled design.jpg")
+    set_background("assets/bg3.jpg")
     
     st.markdown("<h1 class='title-style'>Kushal Maa Platform</h1>", unsafe_allow_html=True)
     st.markdown("<p class='subtitle-style'>An Interactive Platform for Admins and Moderators</p>", unsafe_allow_html=True)
